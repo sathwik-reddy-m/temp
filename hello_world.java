13631ff38e46de76cb05560ec6c8fba511505d53
sathwik-reddy-m <116341087+sathwik-reddy-m@users.noreply.github.com>
2024-06-09 20:39:46 +0530

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,9 @@
+import java.util.*;/**
+ * hello_world
+ */
+public class hello_world {
+    public static void main(String []args)
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ thanks.java
@@ -0,0 +1,6 @@
+public class thanks {
+    public static void main(String []args)
+    {
+        System.out.println("Thanks!");
+    }
+}
